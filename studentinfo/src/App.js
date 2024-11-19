732aaa85cb2e7144272a6785919763f4c979d38f
Logambigan Thiruganeshapathy <138703319+iamloshan@users.noreply.github.com>
2024-11-19 13:27:53 +0000

    profile updated

--- studentinfo/src/App.js
+++ studentinfo/src/App.js
@@ -1,21 +1,21 @@
 import "./App.css";
 import "./assets/css/compo.css";
 import Table from "./components/table";
-import { students } from "./assets/StudentsDb.js";
+import { students } from "./assets/StudentsDb";
 import { useState } from "react";
 import Profile from "./components/profile";
 
 function App() {
-  const [student, setStudentedIndex] = useState(0);
+  const [studentIndex, setStudentIndex] = useState(0);
 
   return (
     <div className="outerDiv">
       <div className="leftDiv">
-        <Table students={students} setStudentedIndex={setStudentedIndex} />
+        <Table students={students} setStudentIndex={setStudentIndex} />
       </div>
 
       <div className="rightDiv">
-        <Profile student={students[studentIndex]}/>
+        <Profile student={students[studentIndex]} />
       </div>
     </div>
   );
--- studentinfo/src/components/profile.js
+++ studentinfo/src/components/profile.js
@@ -1,9 +1,28 @@
 const Profile = ({ student }) => {
-    return (
+  return (
+    <div className="profile">
       <div className="profile">
-        <img src={`/profilepic/${student.profilePic}`} alt={student.firstName} />
+        <img
+          src={`/profilepic/${student.profilePic}`}
+          alt={student.firstName}
+        />
+
+        <table border={1}>
+            <tr>
+                <td>
+                    ID : {student.studentId}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Full Name : {`${student.firstName} ${student.lastName}`}
+                </td>
+            </tr>
+           
+        </table>
       </div>
-    );
-  };
-  
-  export default Profile;
+    </div>
+  );
+};
+
+export default Profile;
